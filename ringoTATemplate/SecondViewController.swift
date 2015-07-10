31cf644c227d48d8ec66f072c062d05a52a50feb
gmittal <gautam@mittal.net>
2015-07-09 17:18:03 -0700

    init awesomeness

---
+++ ringoTATemplate/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  ringoTATemplate
+//
+//  Copyright (c) 2015 Ringo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
